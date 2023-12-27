0995687a3a3a34278c7068f1402525546823ae8e
pnywise <bunayya12345@gmail.com>
2023-12-27 22:03:51 +0700

    compiled proto

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"context"
+	"time"
+
+	social_media_proto "github.com/PNYwise/post-service/proto"
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+)
+
+type postHandler struct {
+	social_media_proto.UnimplementedPostServer
+}
+
+func NewPostHandler() *postHandler {
+	return &postHandler{}
+}
+
+func (p *postHandler) Create(context.Context, *social_media_proto.PostDetail) (*social_media_proto.PostDetail, error) {
+	data := &social_media_proto.PostDetail{}
+	return data, nil
+}
+func (p *postHandler) ReadAllByUserId(context.Context, *social_media_proto.Uuid) (*social_media_proto.PostList, error) {
+	data := &social_media_proto.PostList{}
+	return data, nil
+}
+func (p *postHandler) Delete(context.Context, *social_media_proto.Uuid) (*empty.Empty, error) {
+	return nil, nil
+}
+
 func main() {
-	
+
+	time.Local = time.UTC
+	srv := grpc.NewServer()
+	postHandlers := NewPostHandler()
+	social_media_proto.RegisterPostServer(srv, postHandlers)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/PNYwise/post-service
 
 go 1.20
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231212172506-995d672761c0 // indirect
+	google.golang.org/grpc v1.60.1 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
+)
